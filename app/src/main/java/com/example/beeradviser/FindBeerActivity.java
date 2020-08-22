29e2c8c7cc7b542e628979bce04c2a8f9cf1b8b8
vsafontev <vladislavsafontev@gmail.com>
2020-08-22 20:25:49 +0300

    two commit

--- app/src/main/java/com/example/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/example/beeradviser/FindBeerActivity.java
@@ -7,8 +7,10 @@ import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class FindBeerActivity extends AppCompatActivity {
+import java.util.List;
 
+public class FindBeerActivity extends AppCompatActivity {
+    private BeerExpert expert = new BeerExpert();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +22,12 @@ public class FindBeerActivity extends AppCompatActivity {
         TextView brands = (TextView) findViewById(R.id.brands);
         Spinner color = (Spinner) findViewById(R.id.color);
         String beerType = String.valueOf(color.getSelectedItem());
-        brands.setText(beerType);
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormatted);
 
     }
 }
